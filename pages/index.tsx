b5d7b9b5e1794a3bab63b57b5d7099d0aee2e5ac
sizips32 <58421243+sizips32@users.noreply.github.com>
2025-05-26 09:48:12 +0900

    파비콘 및 _document.tsx 적용, 코드 정리

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
                 <title>승리복음 매일 묵상 플래너</title>
                 <meta name="description" content="골든 서클 방식으로 매일 말씀을 묵상하고 기록하세요." />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.svg" />
             </Head>
             <main>
                 <GoldenCircleForm />
